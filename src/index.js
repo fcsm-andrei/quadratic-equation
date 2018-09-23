c966c46ab3bd90f86280f40832aca985538ee333
fcsm-andrei <andreileonchikov@gmail.com>
2018-09-23 16:34:14 +0300

    4th task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  
+  var arr = equation.split(" ");
+  var a, b, c, D, x1, x2;
+  	
+  //if(arr.length == 9){
+  	a = Number(arr[0]);
+  	b = Number(arr[3] + arr[4]);
+  	c = Number(arr[7] + arr[8]);
+  	D =b*b -4*a*c;
+  	if(D > 0){
+  		x1 =(-b - Math.sqrt(D)) / (2*a);
+  		x2 = (-b + Math.sqrt(D)) / (2*a);
+  	} else if(D == 0){
+  		x1 = x2 = -b / (2*a);
+  	} else {x1 = x2 = null}
+  //}
+ 	  var resolve = [Math.round(x1), Math.round(x2)];
+    function compareNumeric(i, j) {
+      return i - j;
+    }
+    resolve.sort(compareNumeric);
+
+  
+  return resolve;
+  
 }
